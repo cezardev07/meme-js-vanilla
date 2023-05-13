aea7c5dd6509da6b3f5c590c4b20faa75edc10a4
cezardev07 <cezardev07@gmail.com>
2023-05-13 13:34:22 -0300

    feat: add new module javascript

--- js/modules/move/move.js
+++ js/modules/move/move.js
@@ -1,8 +1,8 @@
 import { getSize } from "./get-size/get-size.js";
+import { position } from "./position-btn/position-btn.js";
 
 export const move = () => {
     const btn = document.querySelector(".move");
-
     const size = getSize();
 
     const {width} = size[0];
@@ -11,9 +11,7 @@ export const move = () => {
     const generationRandom = () => {
         const bottom = Math.floor(Math.random() * parseInt(height - 100));
         const left = Math.floor(Math.random() * parseInt(width - 200));
-            btn.style.position = "absolute";
-            btn.style.bottom = bottom + "px";
-            btn.style.left = left + "px";
+        position(btn, bottom, left)
     };
 
     return generationRandom();
