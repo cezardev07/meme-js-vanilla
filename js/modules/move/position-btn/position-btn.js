3fc425619fd1903aa91e6c629716048d3485214a
cezardev07 <cezardev07@gmail.com>
2023-05-13 13:58:13 -0300

    feat: add transition in modulo position ui | ux

--- js/modules/move/position-btn/position-btn.js
+++ js/modules/move/position-btn/position-btn.js
@@ -2,6 +2,7 @@ export const position = (btn, bottom, left) => {
     
     const positionBtn = (btn, bottom, left) => {
         btn.style.position = "absolute";
+        btn.style.transition = "all .07s ease";
         btn.style.bottom = `${bottom}px`;
         btn.style.left = `${left}px`;
     };
